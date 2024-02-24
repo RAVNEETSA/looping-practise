51adb6519a4968a6bbb1cab34aacd5ac2bb59489
RAVNEETSA <160113152+RAVNEETSA@users.noreply.github.com>
2024-02-24 21:50:22 +0530

    Add files via upload

---
+++ looping practise.js
@@ -0,0 +1,29 @@
+
+// let prompt=require("prompt-sync")();
+// var x=prompt("enter  first value");
+// var  y=prompt("enter sectend second");
+// var z=parseInt(x)+parseInt(y);
+// var b=parseInt(x)-parseInt(y);
+// console.log("the sum of "+x+"and "+y+" is: "+z);
+// console.log("the sum of "+x+"and "+y+" is: "+b);
+ 
+// let prompt=require("prompt-sync")();
+// let a=prompt("enter your name:");
+// let b=prompt("enter your last name:");
+// // let c=prompt("last name");
+// if( b=="kaur"){
+//     console.log("your are girl");
+// }
+// if( b=="singh"){
+//     console.log("your are boy");
+// }
+  
+
+const prompt=require("prompt-sync")();
+let d=prompt("enter number:");
+if (  d%2==0){
+    console.log("number is divisible by 2");
+}
+else
+console.log("number is not divisible by 2");
+
